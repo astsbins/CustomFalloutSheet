0015414f77ce2fa6c64a98c2b18935b1a7e2ef5e
jack <astsbins@gmail.com>
2020-05-27 22:33:02 +1000

    very bare bones of alternate layout created

---
+++ generator.js
@@ -0,0 +1,10 @@
+const attribute_mods = {
+    "skills" : ["sneak", "larceny", "pilot","medicine", "survival", "small_guns", "energy", "big guns",
+                "explosivess", "close combat", "throwing", "athletics", "speech",
+                "deception", "barter", "investigate", "search"],
+
+    "crafts" : ["armor","weapons", "food", "gear", "machine", "pwr armor", "robots", "construction"],
+
+    "knowledges" : ["computers", "science", "local", "history", "creatures", "bos", "enclave", "institute", "ceasars_legion", "lone_star",
+                    "supermutants"]
+}
